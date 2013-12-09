fe9784c80a624cf8b28907cc6cab999c41f4b428
Gaurav Kumar <gaurav.bond.itbhu@gmail.com>
2013-12-09 23:26:03 +0530

    Search Page Changes

--- public/js/controllers/search.js
+++ public/js/controllers/search.js
@@ -2,10 +2,10 @@ angular.module('mean.search')
 .controller('SearchController', 
 	['$scope', '$routeParams', '$location','Movies',
 	function ($scope, $routeParams, $location,Movies) {
-		console.log("routeParams coingn "+ $routeParams.query);
-	// $scope.search = function() {
-		console.log("Searching in database for moviename"+$routeParams.query);
-		Movies.query({searchString:$routeParams.query},function(movies) {
+		$scope.searchString = $routeParams.query;
+		console.log("routeParams coingn "+ $scope.searchString);
+		console.log("Searching in database for moviename" + $scope.searchString);
+		Movies.query({searchString: $scope.searchString},function(movies) {
 			console.log("Got result back after querying : "+movies);
 			$scope.movies = movies;
 		});
