2ebd206fe8b817f43ae676d9b57d59b0f0641937
mangal_dev <dev.itbhu@gmail.com>
2013-12-13 01:45:35 +0530

    .

--- public/js/services/movie.js
+++
@@ -1,7 +0,0 @@
-//Movie service used for articles REST endpoint
-angular.module('mean.search').factory("Movies", ['$resource', function($resource) {
-	return $resource('search/:searchString', {searchString: '@_id'
-		    }, 
-		    {update: {method: 'GET'}}
-		);
-}]);
